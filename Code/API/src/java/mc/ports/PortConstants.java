b020256bea936240266480bb5f70a514656fdc10
Sergio Soria <sergio.soria.nieto@gmail.com>
2015-01-05 19:37:26 +0100

    PORTB and PORTC values

--- Code/API/src/java/mc/ports/PortConstants.java
+++ Code/API/src/java/mc/ports/PortConstants.java
@@ -4,9 +4,9 @@ public interface PortConstants {
 
     final static byte PORTA = (byte) 0x00;
 
-    final static byte PORTB = (byte) 0x00;
+    final static byte PORTB = (byte) 0x01;
 
-    final static byte PORTC = (byte) 0x00;
+    final static byte PORTC = (byte) 0x02;
 
     final static byte EVENT_INTERRUPT_0 = (byte) 0x00;
 
